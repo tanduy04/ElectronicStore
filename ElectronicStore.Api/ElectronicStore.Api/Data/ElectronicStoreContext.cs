6f67912d02db3549479e11d9cf27068ef803f85c
Nguyen Tan Duy <duynguyen1918@gmail.com>
2025-10-04 21:35:11 +0700

    fix connectionString

--- ElectronicStore.Api/ElectronicStore.Api/Data/ElectronicStoreContext.cs
+++ ElectronicStore.Api/ElectronicStore.Api/Data/ElectronicStoreContext.cs
@@ -56,8 +56,7 @@ public partial class ElectronicStoreContext : DbContext
     public virtual DbSet<Voucher> Vouchers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=ElectronicStore;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:ElectronicStoreConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
