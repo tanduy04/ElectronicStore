6778aaaa8139c82abcd3a8ea184a8b43e62d8d48
Nguyen Tan Duy <duynguyen1918@gmail.com>
2025-11-30 13:01:22 +0700

    test

--- ElectronicStore.Api/ElectronicStore.Api/Controllers/WeatherForecastController.cs
+++ ElectronicStore.Api/ElectronicStore.Api/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ElectronicStore.Api.Controllers
 {
+    [Authorize(Roles = "Customer")]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
